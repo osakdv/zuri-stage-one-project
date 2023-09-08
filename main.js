01d32d164d2cc9fa8b56a0db103be85a578665c6
osakdv <osakdv@gmail.com>
2023-09-08 22:16:45 +0100

    js adjusted

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
-const currentDay = document.querySelector(".current-day")
-const currentTime = document.querySelector(".current-time")
-
-const date = new Date()
+const currentDay = document.querySelector(".current-day");
+const currentTime = document.querySelector(".current-time");
 
 // Day of the week
-const dayOfTheWeek = date.getDay()
-const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-currentDay.textContent = weekDays[dayOfTheWeek]
+const renderTimeAndDateDom = () => {
+  const date = new Date();
+  const dayOfTheWeek = date.getDay();
+  const weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  currentDay.textContent = weekDays[dayOfTheWeek];
+
+  const UTC = date.toUTCString();
+  const currentTimeInMilliseconds = new Date(UTC).getTime();
+  currentTime.textContent = currentTimeInMilliseconds;
+};
 
 
-// UTC time
-const UTC = date.toUTCString();
-const currentTimeInMilliseconds = new Date(UTC).getTime();
-currentTime.textContent = currentTimeInMilliseconds;
+renderTimeAndDateDom()
+setInterval(renderTimeAndDateDom, 1000);
